9bbbe36bbcd94413f7bb119613b29b361b3c1e97
poletniy <33935110+poletniy@users.noreply.github.com>
2017-12-06 17:25:44 +0300

    Update bot.java

--- bot.java
+++ bot.java
@@ -2,10 +2,10 @@ public class bot {
     public static void main(String[] args) {
         int result = (int) (Math.random()*2);
         if (result == 0) {
-            System.out.println("Законопроект отклонен"); //to annul a law
+            System.out.println("No"); //to annul a law
         }
         else{
-            System.out.println("Законопроект принят"); //to pass a law
+            System.out.println("Yes"); //to pass a law
         }
     }
 }
