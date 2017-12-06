17124926d6901c8d29c66dd3084ca5018d35a955
poletniy <33935110+poletniy@users.noreply.github.com>
2017-12-06 17:48:17 +0300

    Update bot.java

--- bot.java
+++ bot.java
@@ -1,6 +1,6 @@
 public class bot {
     public static void main(String[] args) {
-        int result = (int) (Math.random()*2);
+        int result = (int) (Math.random()*2); //get a 50/50 chance in random generator
         if (result == 0) {
             System.out.println("No"); //to annul a law
         }
